190aee0185056541af1bc3f4828d68e954165b59
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-09-30 16:32:20 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import {BrowserRouter as Router} from "react-router-dom";
 import Hope from "./test/Hope";
 
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
 
 function App() {
   return (
     <div>
       <Router>
     <div className="App">
+        <Header/>
 
         <Hope />
     </div>
         </Router>
-
+        <Footer/>
     </div>
 );
 }
