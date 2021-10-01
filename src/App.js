becdbd52494f6d0f086389718336119d52a65401
gavin <gavingunapala06@gmail.com>
2021-10-01 17:53:12 +0530

    view Payment details page created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Hope from "./test/Hope";
-
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import AdminSideNav from "./SideNav/AdminSideNav";
 import JourneyReport from "./Reports/JourneyReport";
 import StartJourney from "./Reports/StartJourney";
-import AddCredit from "./Payment/AddCredit";
+
 import GetPhysicalToken from "./Customer/GetPhysicalToken";
 import BusRoute from "./Bus/BusRoute";
+import ViewPayment from "./Payment/ViewPayment";
 
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Route exact path="/JourneyReport" component={JourneyReport}/>
         <Route exact path="/Hope" component={Hope}/>
         <Route exact path="/StartJourney" component={StartJourney}/>
-        <Route exact path="/AddCredit" component={AddCredit}/>
+        <Route exact path="/ViewPayment" component={ViewPayment}/>
         <Route exact path="/GetPhysicalToken" component={GetPhysicalToken}/>
         <Route exact path="/BusRoute" component={BusRoute}/>
 
